dac051799624767a120f39dcb17174260b8d0004
fv17 <forest.valley0417@gmail.com>
2019-08-18 09:01:12 +0900

    study: ES6 Arrow Functions(Optional)

--- src/App.js
+++ src/App.js
@@ -21,22 +21,37 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Welcome to the Road to learn React</h2>
-      {list.map(item => 
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      )}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>Welcome to the Road to learn React</h2>
+//       {list.map(item => 
+//         <div key={item.objectID}>
+//           <span>
+//             <a href={item.url}>{item.title}</a>
+//           </span>
+//           <span>{item.author}</span>
+//           <span>{item.num_comments}</span>
+//           <span>{item.points}</span>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+const App= () => 
+<div className="App">
+  <h2>Welcome to the Road to learn React</h2>
+  {list.map(item => 
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
     </div>
-  );
-}
+  )}
+</div>
 
 export default App;
