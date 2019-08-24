f623d9e82406c0b5f97202cf03a1969958443988
fv17 <forest.valley0417@gmail.com>
2019-08-24 10:37:50 +0900

    study: Reusable Components

--- src/App.js
+++ src/App.js
@@ -95,16 +95,35 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button 
+              <Button 
                 onClick={() => onDismiss(item.objectID)}
-                type="button"
               >
                 dismiss  
-              </button>
+              </Button>
             </span>
           </div>
         )}
       </>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    const { 
+      onClick,
+      className = '',
+      children
+    } = this.props;
+
+    return(
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
     )
   }
 }
