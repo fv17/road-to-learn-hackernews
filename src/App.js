28ab00da700f1ce0b74e1f6c34a9184cc71e431e
fv17 <forest.valley0417@gmail.com>
2019-08-24 11:00:57 +0900

    study: Component Declarations

--- src/App.js
+++ src/App.js
@@ -65,87 +65,46 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props
-    return(
-      <form>
-        {children} <input 
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    )
-  }
-}
+// ES6 arrow functions let you remove the block body, {}
+const Search = ({ value, onChange, children }) => 
+  <form>
+    {children} <input 
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-class Table extends Component {
-  render() {
-    const { list, searchTerm, onDismiss } = this.props
-    return(
-      <>
-        <h2>Welcome to the Road to learn React</h2>
-        {list.filter(isSearched(searchTerm)).map(item => 
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button 
-                onClick={() => onDismiss(item.objectID)}
-              >
-                dismiss  
-              </Button>
-            </span>
-          </div>
-        )}
-      </>
-    )
-  }
-}
+const Table = ({ list, searchTerm, onDismiss }) => 
+  <>
+    <h2>Welcome to the Road to learn React</h2>
+    {list.filter(isSearched(searchTerm)).map(item => 
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <Button 
+          onClick={() => onDismiss(item.objectID)}
+        >
+          dismiss  
+        </Button>
+      </span>
+    </div>
+    )}
+  </>
 
-class Button extends Component {
-  render() {
-    const { 
-      onClick,
-      className = '',
-      children
-    } = this.props;
-
-    return(
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    )
-  }
-}
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h2>Welcome to the Road to learn React</h2>
-//       {list.map(item => 
-//         <div key={item.objectID}>
-//           <span>
-//             <a href={item.url}>{item.title}</a>
-//           </span>
-//           <span>{item.author}</span>
-//           <span>{item.num_comments}</span>
-//           <span>{item.points}</span>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 // const App= () => 
 // <div className="App">
