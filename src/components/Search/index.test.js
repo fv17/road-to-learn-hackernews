996dfe700276120f9d743e75dc5ac679e1eab8ae
fv17 <forest.valley0417@gmail.com>
2019-09-04 19:55:00 +0900

    study: Snapshot Tests with Jest

--- src/components/Search/index.test.js
+++ src/components/Search/index.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './components/App';
+import Search from '.';
 
-describe('App', () => {
+describe('Search', () => {
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<Search />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('has a valid snapshot', () => {
     const component = renderer.create(
-      <App />
+      <Search>Search</Search>
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
